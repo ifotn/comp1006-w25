96c5b89635d89887bb17ad759167042eddccda27
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-01-21 10:26:46 -0500

    l3 / 2 - destination form w/country dropdown

---
+++ destination.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Destination</title>
+</head>
+<body>
+    <?php include ('shared/header.php'); ?>
+    <h1>Destination Details</h1>
+    <form method="post" action="save-destination.php">
+    <fieldset>
+        <label for="name">Name:</label>
+        <input name="name" />
+    </fieldset>
+    <fieldset>
+        <label for="attractions">Attractions:</label>
+        <textarea name="attractions"></textarea>
+    </fieldset>
+    <fieldset>
+        <label for="countryId">Country:</label>
+        <select name="countryId">
+            <?php
+            // connect
+            include ('shared/db.php');
+
+            // fetch & store country list from db
+            $sql = "SELECT * FROM countries ORDER BY name ASC";
+            $cmd = $db->prepare($sql);
+            $cmd->execute();
+            $countries = $cmd->fetchAll();
+
+            // add each country to dropdown
+            foreach ($countries as $country) {
+                echo '<option value="' . $country['countryId'] . '">' . $country['name'] . '</option>';
+            }
+
+            // disconnect
+            $db = null;
+            ?>
+        </select>
+    </fieldset>
+    <fieldset>
+        <label for="visited">Visited:</label>
+        <input type="checkbox" name="visited" />
+    </fieldset>
+    <button>Save</button>
+    </form>
+</body>
+</html>
