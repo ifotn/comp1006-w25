4ac23b66c4381bdc6f762d528074d44637bb00b0
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-02-18 10:42:02 -0500

    l7/5 - login form

--- login.php
+++ login.php
@@ -1,7 +1,18 @@
 <?php 
-    $title = 'Login';
-    include('shared/header.php'); ?>
-    <h1>Login</h1>
+$title = 'Login';
+include('shared/header.php'); ?>
+<h1>Login</h1>
+<form method="post" action="check-login.php">
+    <fieldset>
+        <label for="username">Email:</label>
+        <input name="username" required type="email" maxlength="50" />
+    </fieldset>
+    <fieldset>
+        <label for="password">Password:</label>
+        <input name="password" required type="password" maxlength="50" />
+    </fieldset>
+    <button>Login</button>
+</form>
 </main>
 </body>
 </html>
--- register.php
+++ register.php
@@ -5,16 +5,16 @@
     <form method="post" action="insert-user.php">
         <fieldset>
             <label for="username">Email:</label>
-            <input name="username" type="email" required id="username" />
+            <input name="username" type="email" required id="username" maxlength="50" />
         </fieldset>
         <fieldset>
             <label for="password">Password:</label>
-            <input name="password" type="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}" id="password" />
+            <input name="password" type="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}" id="password" maxlength="50" />
             <span id="pwMsg"></span>
         </fieldset>
         <fieldset>
             <label for="confirm">Confirm Password:</label>
-            <input name="confirm" type="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}" id="confirm" onkeyup="return comparePasswords();" />
+            <input name="confirm" type="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}" id="confirm" onkeyup="return comparePasswords();" maxlength="50" />
         </fieldset>
         <button onclick="return comparePasswords();">Register</button>
     </form>
