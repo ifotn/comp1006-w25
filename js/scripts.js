439ac3c083a737bf5c5867b84f42aeb16235f803
ifotn <Rich.Freeman@GeorgianCollege.ca>
2025-02-18 09:50:25 -0500

    l7/1 - start strong passwords

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 function confirmDelete() {
     return confirm('Are you sure you want to delete this?');
 }
+
+function comparePasswords() {
+    let pw1 = document.getElementById('password').value;
+    let pw2 = document.getElementById('confirm').value;
+    let pwMsg = document.getElementById('pwMsg');
+
+    // compare
+    if (pw1 == pw2) {
+        pwMsg.innerHTML = '';
+        return true;
+    }
+    else {
+        pwMsg.innerText = 'Passwords do not match';
+        return false;
+    }
+}
--- register.php
+++ register.php
@@ -4,14 +4,19 @@
     <h1>Register</h1>
     <form method="post" action="insert-user.php">
         <fieldset>
-            <label for="username">Username:</label>
-            <input name="username" />
+            <label for="username">Email:</label>
+            <input name="username" type="email" required id="username" />
         </fieldset>
         <fieldset>
             <label for="password">Password:</label>
-            <input name="password" type="password" />
+            <input name="password" type="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}" id="password" />
+            <span id="pwMsg"></span>
         </fieldset>
-        <button>Register</button>
+        <fieldset>
+            <label for="confirm">Confirm Password:</label>
+            <input name="confirm" type="password" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}" id="confirm" onkeyup="return comparePasswords();" />
+        </fieldset>
+        <button onclick="return comparePasswords();">Register</button>
     </form>
 </main>
 </body>
